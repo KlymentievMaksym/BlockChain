34eb5362f597f473d9b2a8b3f88eea6df8a34453
Dadmon <113892002+KlymentievMaksym@users.noreply.github.com>
2026-03-05 17:42:51 +0200

    Update KMA.t.sol
    Покрито усе, чи майже усе, тестами

--- Labs/Lab4/test/KMA.t.sol
+++ Labs/Lab4/test/KMA.t.sol
@@ -2,16 +2,21 @@
 pragma solidity ^0.8.30;
 
 import {Test} from "forge-std/Test.sol";
+import {IERC721Receiver} from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
+
 import {KMA} from "../src/KMA.sol";
 
-contract KMATest is Test
+contract KMATest is Test, IERC721Receiver
 {
     KMA public kma;
     address public user1;
     address public user2;
 
-    // uint256 constant PRECISION = 10 ** 18;
+    function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4)
+    {
+        return IERC721Receiver.onERC721Received.selector;
+    }
 
     function setUp() public
     {
@@ -24,7 +29,67 @@ contract KMATest is Test
     {
         assertEq(kma.name(), "Klymentiev Maksym Andriyovych");
         assertEq(kma.symbol(), "KMA");
-        // assertEq(cave.totalSupply(), 10_000 * PRECISION);
-        // assertEq(cave.balanceOf(address(this)), 10_000 * PRECISION);
+    }
+
+    function test_Mint() public
+    {
+        uint256 picId = kma.createPic("ipfs://bafkreierayyyuv3wqjs4bplqlertcyn7umiorvripu6fb66i6anmy44ehu");
+        assertEq(kma.ownerOf(picId), address(this));
+        assertEq(kma.balanceOf(address(this)), 1);
+    }
+
+    function test_MintToUserCreate() public
+    {
+        uint256 picId = kma.createPic(user1, "ipfs://bafkreierayyyuv3wqjs4bplqlertcyn7umiorvripu6fb66i6anmy44ehu");
+        assertEq(kma.ownerOf(picId), user1);
+    }
+
+    function test_MintToUserTransfer() public
+    {
+        uint256 picId = kma.createPic("ipfs://bafkreierayyyuv3wqjs4bplqlertcyn7umiorvripu6fb66i6anmy44ehu");
+        kma.safeTransferFrom(address(this), user1, picId);
+        assertEq(kma.ownerOf(picId), user1);
+    }
+
+    function test_Creator() public
+    {
+        kma.setCreator(user1);
+    
+        vm.prank(user1);
+        uint256 picId = kma.createPic("ipfs://bafkreierayyyuv3wqjs4bplqlertcyn7umiorvripu6fb66i6anmy44ehu");
+    
+        assertEq(kma.ownerOf(picId), user1);
+    }
+
+    function test_CreatorDelete() public
+    {
+        kma.setCreator(user1);
+    
+        vm.prank(user1);
+        kma.createPic("ipfs://bafkreierayyyuv3wqjs4bplqlertcyn7umiorvripu6fb66i6anmy44ehu");
+
+        kma.deleteCreator(user1);
+        vm.expectRevert(bytes("No permission"));
+        vm.prank(user1);
+        kma.createPic("ipfs://bafkreierayyyuv3wqjs4bplqlertcyn7umiorvripu6fb66i6anmy44ehu");
+    }
+
+    function test_CreatorWrong() public
+    {
+        vm.expectRevert(abi.encodeWithSelector(IAccessControl.AccessControlUnauthorizedAccount.selector, user1, kma.OWNER_ROLE()));
+        vm.prank(user1);
+        kma.setCreator(user1);
+    }
+
+    function test_CreatorNFTTransfer() public
+    {
+        kma.setCreator(user1);
+    
+        vm.prank(user1);
+        uint256 picId = kma.createPic("ipfs://bafkreierayyyuv3wqjs4bplqlertcyn7umiorvripu6fb66i6anmy44ehu");
+        vm.prank(user1);
+        kma.safeTransferFrom(user1, user2, picId);
+    
+        assertEq(kma.ownerOf(picId), user2);
     }
 }
